edd39d0eda05cbfe21bb244fd9c5f71d0c0d1cff
Khalid Sayfullah <nh.muhammadkhalidsayfullah@gmail.com>
2025-06-29 11:41:42 +0100

    Remove Gallery Section

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
 import Menu from './components/Menu';
-import Gallery from './components/Gallery';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 import StickyCallButton from './components/StickyCallButton';
@@ -29,7 +28,6 @@ function App() {
         <Hero />
         <About />
         <Menu onAddToBasket={addItem} />
-        <Gallery />
         <Testimonials />
         <Contact />
       </main>
